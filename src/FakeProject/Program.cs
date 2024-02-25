1d7c188d99eba2238e00e584522e47b5950cc4d3
kstatz12 <karl.statz@gmail.com>
2024-02-25 02:45:22 -0600

    finished subtract feature

--- src/FakeProject/Program.cs
+++ src/FakeProject/Program.cs
@@ -3,11 +3,17 @@
     private static void Main(string[] args)
     {
         int result = Add(1, 2);
+        int result2 = Subtract(1, result);
         Console.WriteLine($"The result is {result}");
     }
 
     private static int Add(int a, int b)
     {
         return a + b;
+    }
+
+    private static int Subtract(int a, int b)
+    {
+        return a - b;
     }
 }
